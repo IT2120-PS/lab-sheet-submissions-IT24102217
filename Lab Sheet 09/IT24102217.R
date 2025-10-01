9b4c855f4b78b4550c4ddb9859bdeaaca0120ee5
IT24102217 <IT24102217@my.sliit.lk>
2025-10-01 18:53:26 +0530

    IT24102217_lab_9

---
+++ Lab Sheet 09/IT24102217.R
@@ -0,0 +1,13 @@
+getwd()
+setwd("C:\\Users\\User\\Desktop\\IT24102217")
+getwd()
+
+#Exercise
+# 1)
+#i)
+baking_time <- rnorm(25, mean = 45, sd = 2)
+
+#ii)
+t.test(baking_time, mu = 46, alternative = "less")
+
+
